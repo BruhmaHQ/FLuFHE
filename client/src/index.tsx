60fb832321efd52281a4fbbf74c0c9e13fa7f7d9
Pratham Bhatnagar <prathambhatnagar2803@gmai.com>
2023-12-07 03:21:59 +0530

    :sparkles: Initiated Client

---
+++ client/src/index.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { WagmiConfig, createConfig } from "wagmi";
+import { ConnectKitProvider, getDefaultConfig } from "connectkit";
+
+const config = createConfig(
+  getDefaultConfig({
+    alchemyId: process.env.REACT_APP_ALCHEMY_ID,
+    walletConnectProjectId: process.env.REACT_APP_WALLETCONNECT_PROJECT_ID as string,
+    appName: "FLuFHE",
+    appDescription: "Federated Learning using Fully Homorphic encryption",
+    appUrl: "https://family.co", 
+    appIcon: "https://family.co/logo.png", 
+  })
+);
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <WagmiConfig config={config}>
+      <ConnectKitProvider>
+        <App />
+      </ConnectKitProvider>
+    </WagmiConfig>
+  </React.StrictMode>
+);
