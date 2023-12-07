9704bd3e09c69078853964b915e6402a3b9fc52f
Pratham Bhatnagar <prathambhatnagar2803@gmai.com>
2023-12-07 05:42:37 +0530

    :sparkles: Add Deploy FL Smart Contract via Client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,29 +1,45 @@
-import { ConnectKitButton } from "connectkit";
 import "cal-sans";
 import { Route } from "wouter";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
-import { useAccount, useWaitForTransaction, useWalletClient } from "wagmi";
+import { useAccount, useWaitForTransaction, useWalletClient,useContractRead, usePrepareContractWrite, useContractWrite } from "wagmi";
 import { FluFLEConfig } from "./utils/flufheConfig";
-
-import {
-  Hash,
-  TransactionReceipt,
-} from "viem";
+import { Hash } from "viem";
 import { Inco } from "./utils/IncoChainConfig";
 import { useEffect, useState } from "react";
+
+const FLModelConfig = {
+  address: "", // whatever will come after deploying
+  abi: FluFLEConfig.abi,
+  chainId: Inco.id,
+};
+
+
+// const { config: FlData } = usePrepareContractWrite({
+//  ...FLModelConfig,
+//  functionName:"addEncrypyedValue",
+//  args: ['0x','0x'],
+// });
+
+// const {
+//   data: data,
+//   write: add,
+//   isLoading: isEncyLoading,
+//   isSuccess: IsEncyStarted,
+//   error: burnErrorCredit,
+// } = useContractWrite(FlData);
 
 function App() {
   const { address } = useAccount();
   const [hash, setHash] = useState<Hash>();
-  const {isSuccess, data} = useWaitForTransaction({
+  const { isSuccess, data } = useWaitForTransaction({
     hash: hash,
     onSuccess(data) {
       console.log("Success", data);
     },
   });
-  const [receipt, setReceipt] = useState<TransactionReceipt>();
+
   const {
     data: walletClient,
     isError,
@@ -39,33 +55,23 @@ function App() {
       args: [false],
       bytecode: `0x${FluFLEConfig.bytocode}`,
     });
-    console.log(hash)
+    console.log(hash);
     setHash(hash);
   };
 
-  // const wait = async () => {
-  //   if (hash) {
-      
-  //   }
-  // }
   useEffect(() => {
-if (isSuccess) console.log({data})
+    if (isSuccess) console.log({ data });
   }, [isSuccess]);
   return (
     <div className="App">
-
-
-<ConnectButton />
+      <ConnectButton />
       <button onClick={() => deployContract()}>deploy</button>
       <div className="mt-[80px]">
-      <Navbar
-      />
+        <Navbar />
         <Route path="/">
           <Home />
         </Route>
       </div>
-
-
     </div>
   );
 }
